4a09599128032d712bfbafe088990b166cb15d9a
HromekFr <hromekfr2@gmail.com>
2021-05-29 11:05:42 +0200

    Laravel from Scratch - Episode 19

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 - Not Found</title>
+</head>
+<body style="display: flex; flex-direction: column; align-content: center; justify-content: center">
+    <img src="{{url('/images/404.jpg')}}" alt="404" style="display: block; margin-left: auto; margin-right: auto; width: 640px; height: 640px;"/>
+    <a href="/" style="text-align: center"><button >Zpět na úvodní stránku</button></a>
+</body>
+</html>
